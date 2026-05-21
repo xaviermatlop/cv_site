faa874aa1f1a0772f561fbccdb45cb6434e3333d
xaviermatlop <xaviermatlop@campus.monlau.com>
2026-05-21 18:04:40 +0200

    ns

--- api/routes/cv.js
+++ api/routes/cv.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => { 
-  return res.json({ 
-    id: 1, 
-    nombre: "Xavier Matep", 
-    profesion: "Desarrollador Web", 
-    experiencia: "Despliegue automatizado con Jenkins y Docker superado con éxito." 
-  }); 
-}); 
+router.get('/', (req, res) => {
+  return res.json({
+    id: 1,
+    nombre: "Xavier Mateo",
+    profesion: "Desarrollador Web",
+    experiencia: "Despliegue automatizado con Jenkins y Docker superado con éxito."
+  });
+});
 
 module.exports = router;
